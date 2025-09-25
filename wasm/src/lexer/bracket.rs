ea992889bf158c2e97d66828484c89b4124cf740
azyarashi <azyarashi@geamz.jp>
2025-09-25 16:52:38 +0900

    特殊な記号の判定を追加

--- wasm/src/lexer/bracket.rs
+++ wasm/src/lexer/bracket.rs
@@ -1,3 +1,5 @@
+use unicode_script::{Script, UnicodeScript};
+
 use typst::syntax::SyntaxKind;
 
 #[derive(Debug, Clone)]
@@ -88,12 +90,14 @@ pub fn bracket_lexer(src: &str) -> Vec<BracketToken> {
                 kind,
                 SyntaxKind::LeftParen | SyntaxKind::LeftBracket | SyntaxKind::LeftBrace
             );
+
+            let adjusted_byte = adjust_byte_position(src, i);
             let (line, column) = calc_line_col(src, i);
 
             ret.push(BracketToken {
                 kind,
                 open,
-                byte: i,
+                byte: adjusted_byte,
                 line,
                 column,
             });
@@ -108,7 +112,13 @@ fn calc_line_col(src: &str, byte: usize) -> (usize, usize) {
     let mut idx = 0;
 
     for c in src.chars() {
-        if idx >= byte {
+        let char_len = c.len_utf8();
+
+        if idx + char_len > byte {
+            if c == '\n' {
+                line += 1;
+                col = 0;
+            }
             break;
         }
 
@@ -116,11 +126,57 @@ fn calc_line_col(src: &str, byte: usize) -> (usize, usize) {
             line += 1;
             col = 0;
         } else {
-            col += 1;
+            if is_emoji(c) {
+                col += 2;
+            } else {
+                col += 1;
+            }
         }
 
-        idx += c.len_utf8();
+        idx += char_len;
     }
 
     (line, col)
+}
+
+fn adjust_byte_position(src: &str, byte: usize) -> usize {
+    let mut adjusted_byte = byte;
+    let mut idx = 0;
+
+    for c in src.chars() {
+        if idx >= byte {
+            break;
+        }
+
+        let char_len = c.len_utf8();
+
+        if char_len > 1 && idx + char_len > byte {
+            adjusted_byte += char_len - 1;
+            break;
+        }
+
+        idx += char_len;
+    }
+
+    adjusted_byte
+}
+
+fn is_emoji(c: char) -> bool {
+    let script = c.script();
+
+    if script == Script::Common {
+        let code = c as u32;
+        matches!(
+            code,
+            0x1F300..=0x1F5FF | // その他の記号及び絵文字
+            0x1F600..=0x1F64F | // 顔文字
+            0x1F680..=0x1F6FF | // 交通及び地図の記号
+            0x1F900..=0x1F9FF | // 補助記号及び絵文字
+            0x2600..=0x26FF   | // 雑多記号
+            0x2700..=0x27BF   | // 装飾記号
+            0xFE00..=0xFE0F     // 異体字セレクタ
+        )
+    } else {
+        false
+    }
 }
